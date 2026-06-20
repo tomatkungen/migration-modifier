17a6be7d3ac83a49b897322de56c5220ae734330
Tomatkungen <mr_a2@hotmail.com>
2026-06-20 18:40:31 +0200

    feat: refactor TypeScript AST handling for return type extraction and improve variable declaration processing

--- src/ast-typescript/core/ts_node_return_type.ts
+++ src/ast-typescript/core/ts_node_return_type.ts
@@ -1,31 +1,45 @@
 import ts from "typescript";
 import { ts_node_export } from "./ts_node_export";
 
-export const ts_node_return_type = (ts_node: ts.Node, checker: ts.TypeChecker): string | undefined => {
+export const ts_node_return_type = (tsNode: ts.Node, tsTypeChecker: ts.TypeChecker): string | undefined => {
 
-    if (ts.isVariableDeclaration(ts_node) && ts.isCatchClause(ts_node.parent))
-        return ts_node_catch_clause(ts_node.parent, checker);
+    if (ts.isVariableDeclaration(tsNode) && ts.isCatchClause(tsNode.parent))
+        return ts_node_catch_clause(tsNode.parent, tsTypeChecker);
 
-    if (ts.isVariableDeclaration(ts_node))
-        return ts_node_variable_declaration(ts_node, checker)
+    if (ts.isVariableDeclaration(tsNode))
+        return ts_node_variable_declaration(tsNode, tsTypeChecker)
 
-    if (ts.isFunctionDeclaration(ts_node))
-        return ts_node_function_declaration(ts_node, checker)
+    if (ts.isFunctionDeclaration(tsNode))
+        return ts_node_function_declaration(tsNode, tsTypeChecker)
+
+    if (ts.isJsxSelfClosingElement(tsNode) || ts.isJsxOpeningElement(tsNode))
+        return ts_node_jsx_element(tsNode)
 }
 
-const ts_node_catch_clause = (tsNode: ts.Node, tsChecker: ts.TypeChecker) => {
+
+
+/**
+ * 
+ * try { } catch (error) { } 
+ * error is variableDeclaraton
+ * 
+ * @param tsNode 
+ * @param tsTypeChecker 
+ * @returns 
+ */
+const ts_node_catch_clause = (tsNode: ts.Node, tsTypeChecker: ts.TypeChecker) => {
     if (!ts.isCatchClause(tsNode))
         return;
 
     const variable = tsNode.variableDeclaration;
     if (!variable) return undefined;
 
-    const symbol = tsChecker.getSymbolAtLocation(variable.name);
+    const symbol = tsTypeChecker.getSymbolAtLocation(variable.name);
     if (!symbol) return undefined;
 
-    const tsType = tsChecker.getTypeOfSymbolAtLocation(symbol, variable.name);
+    const tsType = tsTypeChecker.getTypeOfSymbolAtLocation(symbol, variable.name);
 
-    return tsChecker.typeToString(tsType);
+    return tsTypeChecker.typeToString(tsType);
 }
 
 /**
@@ -43,47 +57,57 @@ const ts_node_catch_clause = (tsNode: ts.Node, tsChecker: ts.TypeChecker) => {
  *
  * @returns NODE_TS
 */
-const ts_node_variable_declaration = (tsNode: ts.Node, tsChecker: ts.TypeChecker): string | undefined => {
+const ts_node_variable_declaration = (tsNode: ts.Node, tsTypeChecker: ts.TypeChecker): string | undefined => {
     if (!ts.isVariableDeclaration(tsNode) || !tsNode.initializer) return;
+
+    /*
+        const w = (tag.current.input as HTMLInputElement).value
+        // .value is the initalizer as property'AccessExpression
+    */
+    if (ts.isPropertyAccessExpression(tsNode.initializer)) {
+        const tsType = tsTypeChecker.getTypeAtLocation(tsNode.initializer)
+        
+        return tsTypeChecker.typeToString(tsType)
+    }
 
     /*
         function foo() { return { x: 123 }; }
         const a = foo(); // foo() the initializer as callExpression
     */
     if (ts.isCallExpression(tsNode.initializer)) {
-        const signature = tsChecker.getResolvedSignature(tsNode.initializer);
+        const signature = tsTypeChecker.getResolvedSignature(tsNode.initializer);
 
         if (signature) {
-            const returnType = tsChecker.getReturnTypeOfSignature(signature);
-            return tsChecker.typeToString(returnType)
+            const returnType = tsTypeChecker.getReturnTypeOfSignature(signature);
+            return tsTypeChecker.typeToString(returnType)
         }
     }
 
     // Get the type of the initializer
-    const type = tsChecker.getTypeAtLocation(tsNode.initializer);
+    const type = tsTypeChecker.getTypeAtLocation(tsNode.initializer);
 
     // If it's a function, get its signature
     const signatures = type.getCallSignatures();
-    if (signatures.length === 0) return ts_node_element_declaration(tsNode, tsChecker);
+    if (signatures.length === 0) return ts_node_element_declaration(tsNode, tsTypeChecker);
 
     // Return the function's return type
     const tsType = signatures[0].getReturnType();
 
-    return tsChecker.typeToString(tsType);
+    return tsTypeChecker.typeToString(tsType);
 }
 
-const ts_node_element_declaration = (tsNode: ts.Node, tsChecker: ts.TypeChecker) => {
-    const symbol = tsChecker.getSymbolAtLocation(tsNode);
+const ts_node_element_declaration = (tsNode: ts.Node, tsTypeChecker: ts.TypeChecker) => {
+    const symbol = tsTypeChecker.getSymbolAtLocation(tsNode);
     const decl = symbol?.declarations?.[0];
 
     if (decl && ts.isVariableDeclaration(decl)) {
-        const type = tsChecker.getTypeAtLocation(decl.name);
+        const type = tsTypeChecker.getTypeAtLocation(decl.name);
         const signatures = type.getCallSignatures();
 
         if (signatures.length) {
-            const returnType = tsChecker.getReturnTypeOfSignature(signatures[0]);
+            const returnType = tsTypeChecker.getReturnTypeOfSignature(signatures[0]);
 
-            return tsChecker.typeToString(returnType);
+            return tsTypeChecker.typeToString(returnType);
             // console.log('1:', ts_node_text(childNode, tsSource), '=>', checker.typeToString(returnType));
         }
     }
@@ -95,43 +119,43 @@ const ts_node_element_declaration = (tsNode: ts.Node, tsChecker: ts.TypeChecker)
  * function App() { return (<div></div>) }
  * 
  * @param ts_node 
- * @param checker 
+ * @param tsTypeDhecker 
  * @returns 
  */
-const ts_node_function_declaration = (ts_node: ts.Node, checker: ts.TypeChecker) => {
+const ts_node_function_declaration = (ts_node: ts.Node, tsTypeDhecker: ts.TypeChecker) => {
     if (!ts.isFunctionDeclaration(ts_node)) return;
 
-    const tsSignature = checker.getSignatureFromDeclaration(ts_node);
+    const tsSignature = tsTypeDhecker.getSignatureFromDeclaration(ts_node);
     if (!tsSignature) return;
 
     const tsType = tsSignature.getReturnType();
 
-    return checker.typeToString(tsType);
+    return tsTypeDhecker.typeToString(tsType);
 }
 
 // test - it take tag elemens as variable declaration
-export const ts_scan_react = (checker: ts.TypeChecker,/*tsProgram: ts.Program,*/ tsNode: ts.Node, tsSource: ts.SourceFile) => {
+export const ts_scan_react = (tsTypeChecker: ts.TypeChecker,/*tsProgram: ts.Program,*/ tsNode: ts.Node, tsSource: ts.SourceFile) => {
     // const checker = tsProgram.getTypeChecker();
 
     // console.log('---', 'file', tsSource.fileName)
     const loop = (childNode: ts.Node) => {
 
-        const symbol = checker.getSymbolAtLocation(childNode);
+        const symbol = tsTypeChecker.getSymbolAtLocation(childNode);
         const decl = symbol?.declarations?.[0];
 
         if (decl && ts.isVariableDeclaration(decl)) {
             // if (decl && ts.isJsxSelfClosingElement(childNode))
-            const type = checker.getTypeAtLocation(decl.name);
+            const type = tsTypeChecker.getTypeAtLocation(decl.name);
             const signatures = type.getCallSignatures();
 
             if (signatures.length) {
-                const returnType = checker.getReturnTypeOfSignature(signatures[0]);
+                const returnType = tsTypeChecker.getReturnTypeOfSignature(signatures[0]);
                 // console.log('1:', ts_node_text(childNode, tsSource), '=>', checker.typeToString(returnType));
             }
         }
 
         if (ts.isVariableDeclaration(childNode)) {
-            const returnType = ts_node_return_type(childNode, checker);
+            const returnType = ts_node_return_type(childNode, tsTypeChecker);
             if (returnType) {
 
                 console.log(
@@ -141,7 +165,7 @@ export const ts_scan_react = (checker: ts.TypeChecker,/*tsProgram: ts.Program,*/
                     "=>",
                     returnType,
                     'export',
-                    ts_node_export(childNode, checker)
+                    ts_node_export(childNode, tsTypeChecker)
                 );
                 console.log('------')
             }
